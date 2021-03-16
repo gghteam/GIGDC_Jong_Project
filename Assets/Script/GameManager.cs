226e888c059f6fc015823a669a4f9e82b418f273
김민준 <eppurdream@gmail.com>
2021-03-17 00:49:22 +0900

    플레이어 변경 and 드론 삭제

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,182 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet;
-    public GameObject player;
-    private GameObject[] bu;
-    public GameObject dron1;
-    public GameObject dron2;
-
-    private Rigidbody2D rb;
-    private Rigidbody2D prb;
-    private SpriteRenderer SR;
-    private Animator animator;
-
-    public float BulletSpeed;
-    public float playerMaxSpeed;
-    public float JumpPower;
-
-    private int count;
-    private bool b = false;
-    void Start()
-    {
-        bu = new GameObject[70];
-        prb = player.GetComponent<Rigidbody2D>();
-        SR = player.GetComponent<SpriteRenderer>();
-        animator = player.GetComponent<Animator>();
-        for (int n = 0; n < bu.Length; n++)
-        {
-            bu[n] = Instantiate(bullet);
-            bu[n].SetActive(false);
-        }
-    }
-
-    // Update is called once per frame
     private void FixedUpdate()
     {
-        MainFunc();
+        //MainFunc();
     }
+    //private void MainFunc()
+    //{
+    //    Debug.DrawRay(player.transform.position, Vector2.down, new Color(0, 1, 0));
+    //    RaycastHit2D rayHit = Physics2D.Raycast(player.transform.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
+    //    if (prb.velocity.y < 0)
+    //    {
+    //        if (rayHit.collider != null)
+    //        {
+    //            if (rayHit.distance > 0.5f)
+    //            {
+    //                animator.SetBool("IsJumping", false);
+    //            }
+    //            Debug.Log(rayHit.collider.name);
+    //        }
+    //    }
+    //}
 
-    private void Update()
-    {
-        // Z
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Fire();
-        }
-
-        // 점프
-        if (Input.GetButtonDown("Jump") && !animator.GetBool("IsJumping"))
-        {
-            prb.AddForce(new Vector2(0, 10 * JumpPower), ForceMode2D.Impulse);
-            animator.SetBool("IsJumping", true);
-        }
-
-        //방향키 뗏을 때 급격히 멈추게 하는 코드
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            prb.velocity = new Vector2(0, prb.velocity.y);
-        }
-
-        // 플레이어 방향 반전
-        if (Input.GetButton("Horizontal"))
-        {
-            SR.flipX = Input.GetAxisRaw("Horizontal") == -1;
-        }
-
-        // 윗키
-        if (Input.GetKey(KeyCode.UpArrow))
-            b = true;
-        else
-            b = false;
-
-
-        //아랫키 + 나중에 할거
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-
-        }
-
-        // 걷는 애니메이션
-        if (Mathf.Abs(prb.velocity.x) < 0.2f)
-            animator.SetBool("IsWalking", false);
-        else
-            animator.SetBool("IsWalking", true);
-    }
-    private void MainFunc()
-    {
-
-        // 움직이기
-        float f = Input.GetAxisRaw("Horizontal");
-        prb.AddForce(new Vector2(100 * f, 0), ForceMode2D.Impulse);
-
-        //속도 제한 두기 (나중에 스킬을 통해 속도 늘리기)
-        if(prb.velocity.x > playerMaxSpeed)
-        {
-            prb.velocity = new Vector2(playerMaxSpeed, prb.velocity.y);
-        }
-        if(prb.velocity.x < playerMaxSpeed * (-1))
-        {
-            prb.velocity = new Vector2(playerMaxSpeed * (-1), prb.velocity.y);
-        }
-
-        //무한 점프 막기
-        Debug.DrawRay(player.transform.position, Vector2.down, new Color(0, 1, 0));
-        RaycastHit2D rayHit = Physics2D.Raycast(player.transform.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
-        if (prb.velocity.y < 0)
-        {
-            if (rayHit.collider != null)
-            {
-                if (rayHit.distance > 0.5f)
-                {
-                    animator.SetBool("IsJumping", false);
-                }
-                Debug.Log(rayHit.collider.name);
-            }
-        }
-    }
-    private void Fire()
-    {
-        // pool로 최적화를 한 총알 발사 코드
-        for (int i = 0; i < bu.Length; i++)
-        {
-            if (!bu[i].activeSelf)
-            {
-                // 첫번째 드론의 총알 발사 코드
-                bu[i].transform.position = dron1.transform.position;   
-                bu[i].SetActive(true);                                  
-                rb = bu[i].transform.GetComponent<Rigidbody2D>();       
-                if (b)
-                {
-                    rb.AddForce(new Vector2(0, 1000f * BulletSpeed));
-                    bu[i].transform.rotation = Quaternion.Euler(0, 0, 90);
-                }
-                else if (SR.flipX == true)
-                {
-                    rb.AddForce(new Vector2(-1000f * BulletSpeed, 0));
-                    bu[i].transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                else
-                {
-                    rb.AddForce(new Vector2(1000f * BulletSpeed, 0));
-                    bu[i].transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                StartCoroutine(Wait4Sec(bu[i]));                        
-                for (int j = 0; j < bu.Length; j++)                     
-                {
-                    if (!bu[j].activeSelf)
-                    {
-                        // 두번째 코드의 발사 코드
-                        bu[j].transform.position = dron2.transform.position;
-                        bu[j].SetActive(true);
-                        rb = bu[j].transform.GetComponent<Rigidbody2D>();
-                        if (b)
-                        {
-                            rb.AddForce(new Vector2(0, 1000f * BulletSpeed));
-                            bu[j].transform.rotation = Quaternion.Euler(0, 0, 90);
-                        }
-                        else if (SR.flipX == true)
-                        {
-                            rb.AddForce(new Vector2(-1000f * BulletSpeed, 0));
-                            bu[j].transform.rotation = Quaternion.Euler(0, 0, 0);
-                        }
-                        else
-                        {
-                            rb.AddForce(new Vector2(1000f * BulletSpeed, 0));
-                            bu[j].transform.rotation = Quaternion.Euler(0, 0, 0);
-                        }
-                        StartCoroutine(Wait4Sec(bu[j]));
-                        return;
-                    }
-                }
-            }
-            
-        }
-    }
-    IEnumerator Wait4Sec(GameObject gm)
-    {
-        // 발사된 총알이 2초뒤 비활설화 됨
-        yield return new WaitForSeconds(2f);
-        gm.SetActive(false);
-    }
 }
--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,10 +4,98 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private GameManager GM;
-    private void OnCollisionStay2D(Collision2D collision)
+    private Animator anime;
+    private SpriteRenderer SR;
+
+    public float Speed;
+    public float JumpPower;
+    public float MaxSpeed;
+
+    private float x; private float Onesec = 0;
+    private float f = 0; private bool isJumping = false; private bool isDown = false;
+    private Rigidbody2D rigidbody;
+    private void Start()
     {
-        //Debug.Log("실행 중");
-        //GM.check = true;
+        SR = gameObject.GetComponent<SpriteRenderer>();
+        anime = gameObject.GetComponent<Animator>();
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        rigidbody.velocity = (new Vector2(x * Speed, 0));
+
+        if(Input.GetButton("Horizontal"))
+        {
+            Onesec += Time.deltaTime;
+            if (Onesec >= 1)
+                anime.SetBool("IsRuning", true);
+        }
+        else 
+        {
+            Onesec = 0; anime.SetBool("IsRuning", false);
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            SR.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        // 걷는 애니메이션
+        if (!anime.GetBool("IsRuning"))
+        {
+            if (Mathf.Abs(rigidbody.velocity.x) < 0.2f)
+                anime.SetBool("IsWalking", false);
+            else
+                anime.SetBool("IsWalking", true);
+        }
+
+        if (Input.GetKey(KeyCode.X) && !isJumping && !isDown)
+        {
+            f += Time.deltaTime;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 40);
+            Debug.Log(f);
+            if (f >= 0.3f)
+            {
+                isJumping = true; isDown = true;
+                f = 0;
+            }
+            anime.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.X)) { isDown = false; f = 0; }
+
+
+        if (rigidbody.velocity.x > MaxSpeed)
+        {
+            rigidbody.velocity = new Vector2(MaxSpeed, rigidbody.velocity.y);
+        }
+        else if (rigidbody.velocity.x < -MaxSpeed)
+        {
+            rigidbody.velocity = new Vector2(-MaxSpeed, rigidbody.velocity.y);
+        }
+
+        if (rigidbody.velocity.y > JumpPower*3)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpPower * 3);
+        }
+        else if (rigidbody.velocity.y < -JumpPower * 3)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, -JumpPower * 3);
+        }
+
+        Debug.DrawRay(transform.position, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.down, 2, LayerMask.GetMask("Platform"));
+        if(rigidbody.velocity.y < 0.3f)
+        {
+            if(rayHit.collider != null)
+            {
+                if(rayHit.distance > 0.2f)
+                {
+                    anime.SetBool("IsJumping", false); isJumping = false;
+                }
+                Debug.Log(rayHit.collider.name);
+            }
+        }
     }
 }
