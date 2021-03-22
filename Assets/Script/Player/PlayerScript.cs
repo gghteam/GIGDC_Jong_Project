10be67a9f5fd28f1edb851fb6b03ff995372f947
김민준 <eppurdream@gmail.com>
2021-03-22 23:15:22 +0900

    공격 애니메이션 작업1 - 진행중

--- Assets/Script/Player/PlayerScript.cs
+++ Assets/Script/Player/PlayerScript.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private Animator anime;
-    private SpriteRenderer SR;
+    public SpriteRenderer SR;
 
     public float RunSpeed;
     public float JumpPower;
     public float MaxSpeed;
     public float Gravity;
 
-    private float x; private float Onesec = 0; private float temp1 = 0; private float temp2 = 0; private float temp3 = 0;
-    private float f = 0; private bool isJumping = false; private bool isDown = false; private bool isFall = false;
+    private float time1 = 0; private float time2 = 0;
+    
+    private float x = 0;
+    private float temp1 = 0; private float temp2 = 0; private float temp3 = 0;
+    private bool isJumping = false; private bool isDown = false; private bool isFall = false;
     private Rigidbody2D rigidbody;
     private void Start()
     {
@@ -26,15 +29,17 @@ public class PlayerScript : MonoBehaviour
     }
     private void Update()
     {
+        //움직이기
         x = Input.GetAxis("Horizontal");
         rigidbody.velocity = (new Vector2(x * MaxSpeed, 0));
 
+        //0.5초 후 달리기
         if (Input.GetButton("Horizontal"))
         {
             if (!isFall)
             {
-                Onesec += Time.deltaTime;
-                if (Onesec >= 0.5f)
+                time1 += Time.deltaTime;
+                if (time1 >= 0.5f)
                 {
                     anime.SetBool("IsRuning", true);
                 }
@@ -42,9 +47,10 @@ public class PlayerScript : MonoBehaviour
         }
         else
         {
-            Onesec = 0; anime.SetBool("IsRuning", false);
+            time1 = 0; anime.SetBool("IsRuning", false);
         }
 
+        //방향전환
         if (Input.GetButton("Horizontal"))
         {
             SR.flipX = Input.GetAxisRaw("Horizontal") == -1;
@@ -59,16 +65,16 @@ public class PlayerScript : MonoBehaviour
                 anime.SetBool("IsWalking", true);
         }
 
-       
+       //점프
         if (Input.GetKey(KeyCode.X) && !isJumping && !isDown)
         {
-            f += Time.deltaTime;
-            JumpPower -= f * (temp2/10);
+            time2 += Time.deltaTime;
+            JumpPower -= time2 * (temp2/10);
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpPower);
-            if (f >= 0.3f)
+            if (time2 >= 0.3f)
             {
                 isJumping = true; isDown = true;
-                f = 0;
+                time2 = 0;
             }
             anime.SetBool("IsJumping", true);
         }
@@ -77,6 +83,7 @@ public class PlayerScript : MonoBehaviour
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, -Gravity); JumpPower = temp2;
         }
 
+        //느리게 낙하
         if(isJumping && Input.GetKey(KeyCode.X))
         {
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, -5); isFall = true; MaxSpeed = temp1; Debug.Log("적용 중");
@@ -86,13 +93,16 @@ public class PlayerScript : MonoBehaviour
             Gravity = temp3; isFall = false;
         }
 
-        if (Input.GetKeyUp(KeyCode.X)) { isJumping = true; isDown = false; f = 0; JumpPower = temp2; }
+        //점프2
+        if (Input.GetKeyUp(KeyCode.X)) { isJumping = true; isDown = false; time2 = 0; JumpPower = temp2; }
 
+        //달리면 속도 빨라지기
         if (anime.GetBool("IsRuning"))
             MaxSpeed = RunSpeed;
         else
             MaxSpeed = temp1;
 
+        //속도 제한 두기
         if (rigidbody.velocity.x > MaxSpeed)
         {
             rigidbody.velocity = new Vector2(MaxSpeed, rigidbody.velocity.y);
@@ -102,6 +112,7 @@ public class PlayerScript : MonoBehaviour
             rigidbody.velocity = new Vector2(-MaxSpeed, rigidbody.velocity.y);
         }
 
+        //점프, 낙하 속도 제한 두기
         if (rigidbody.velocity.y > JumpPower*3)
         {
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpPower * 3);
@@ -111,8 +122,7 @@ public class PlayerScript : MonoBehaviour
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, -JumpPower * 3);
         }
 
-
-        Debug.Log(MaxSpeed);
+        // 무한으로 즐기는 점프 방지하기
         Debug.DrawRay(transform.position, Vector3.down, new Color(0, 1, 0));
         RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.down, 2, LayerMask.GetMask("Platform"));
         if(rigidbody.velocity.y < 0.3f)
