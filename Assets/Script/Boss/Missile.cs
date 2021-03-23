d7fbac818e488b73a2f11046c239301f33424eb0
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-03-24 00:45:12 +0900

    미사일 풀링,유도 구현

---
+++ Assets/Script/Boss/Missile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    GameObject target;
+    [SerializeField]
+    GameObject missilePrepab;
+    [SerializeField]
+    float speed = 2f, rotSpeed = 2f;
+
+    Quaternion rotTarget;
+    Vector3 dir;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame 시름
+    void Update()
+    {
+        GuidMissile();
+    }
+    void GuidMissile()
+    {
+        dir = (target.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rotTarget = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotTarget, Time.deltaTime * rotSpeed);
+        transform.Translate(Vector2.right*speed*Time.deltaTime);
+    }
+}
---
+++ Assets/Script/Boss/MissileSpawn.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileSpawn : MonoBehaviour
+{
+    public static MissileSpawn instance;
+    public GameObject missilePrepab;
+
+    List<GameObject> missile = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (MissileSpawn.instance == null)
+        {
+            MissileSpawn.instance = this;
+        }
+    }
+    void Start()
+    {
+        CreateMissile(5);
+    }
+    public void CreateMissile(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject _missile = Instantiate(missilePrepab) as GameObject;
+            _missile.transform.parent = this.transform;
+            _missile.SetActive(false);
+            missile.Add(_missile);
+        }
+    }
+    public GameObject GetMissile(Vector3 pos)
+    {
+        GameObject reqMissile = null;
+        for (int i = 0; i < missile.Count; i++)
+        {
+            if (missile[i].activeSelf.Equals(false))
+            {
+                reqMissile = missile[i];
+                break;
+            }
+        }
+        if (reqMissile == null)
+        {
+            GameObject newMissile = Instantiate(missilePrepab) as GameObject;
+            newMissile.transform.parent = this.transform;
+            missile.Add(newMissile);
+            reqMissile = newMissile;
+        }
+        reqMissile.SetActive(true);
+        reqMissile.transform.position = pos;
+        return reqMissile;
+    }
+
+    public void AllClear_Missile()
+    {
+        for (int i = 0; i < missile.Count; i++)
+        {
+            missile[i].SetActive(false);
+        }
+    }
+}
