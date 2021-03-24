34b72e8f474a2a76fde1a9286b2f7307799dfd90
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-03-24 19:22:55 +0900

    일단 미사일로 남자 보스 패턴 2 만듬

--- Assets/Script/Boss/BossBoy.cs
+++ Assets/Script/Boss/BossBoy.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class BossBoy : MonoBehaviour
@@ -7,9 +8,13 @@ public class BossBoy : MonoBehaviour
     [SerializeField]
     private float patternDelay = 0f;
     private float time = 0f;
+
     private GameObject Missile;
+
     public int bossPattern = 0;
     private bool isPattern = false;
+    public bool phase2 = false;
+
    
     // Start is called before the first frame update
     void Start()
@@ -21,17 +26,54 @@ public class BossBoy : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
+        //보스 패턴 증가
         if (patternDelay < time)
         {
             isPattern = true;
             time = 0f;
             bossPattern++;
         }
-        if (bossPattern == 1&& isPattern)
+        
+        if (isPattern)
         {
-            Invoke("ShootMissile", patternDelay);
-            isPattern = false;
+            switch (bossPattern)
+            {
+                case 1:
+                    //패턴 1 페이즈2
+                    if (phase2)
+                    {
+                        Invoke("ShootMissile", 0.1f);
+                        Invoke("ShootMissile", 0.3f);
+                        Invoke("ShootMissile", 0.5f);
+                        Invoke("ShootMissile", 0.7f);
+                        isPattern = false;
+                    }
+                    //패턴 1 미사일 발사
+                    else
+                    {
+                        ShootMissile();
+                        isPattern = false;
+                    }
+                    break;
+                case 2:
+                    ShootBullet();
+                    Debug.Log("공격2");
+                    isPattern = false;
+                    break;
+                case 3:
+                    Debug.Log("공격3");
+                    isPattern = false;
+                    break;
+                case 4:
+                    Debug.Log("공격4");
+                    isPattern = false;
+                    break;
+
+            }
+
         }
+
+        //패턴 4까지 다 돌면 0으로 초기화
         if (bossPattern > 4)
         {
             bossPattern = 0;
@@ -41,5 +83,14 @@ public class BossBoy : MonoBehaviour
     {
         Missile = MissileSpawn.instance.GetMissile(this.transform.position);
 
+    }
+    void ShootBullet()
+    {
+        for (int i = 0; i < 360; i += 13)
+        {
+            Missile = MissileSpawn.instance.GetMissile(this.transform.position);
+            //Z에 값이 변해야 회전이 이루어지므로, Z에 i를 대입한다.
+            Missile.transform.rotation = Quaternion.Euler(0, 0, i);
+        }
     }
 }
--- Assets/Script/Boss/Missile.cs
+++ Assets/Script/Boss/Missile.cs
@@ -39,7 +39,7 @@ public class Missile : MonoBehaviour
         this.gameObject.SetActive(false);
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
