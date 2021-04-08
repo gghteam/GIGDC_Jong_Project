4d7d0ef82ca506c7313f002687d67517a9fbfc95
Anglik <seonwoo6429@gmail.com>
2021-04-08 19:32:37 +0900

    UI스크립트 개편 + Quit 버튼스크립트 추가

--- Assets/Script/UI_Script/UI_Manager.cs
+++ Assets/Script/UI_Script/UI_Manager.cs
@@ -19,6 +19,7 @@ public class Pause
         }
     }
 
+    //일시정지 체크를 위한 싱글톤
     public bool pause_Check = false;
 }
 public class UI_Manager : MonoBehaviour
@@ -46,4 +47,27 @@ public class UI_Manager : MonoBehaviour
             }
         }
     }
+
+    #region
+    //버튼에서 오브젝트 키고 끄는 함수
+    public void SetActive_false(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    public void SetActive_true(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    #endregion
+
+    #region
+    //퍼즈 끄는 함수
+    public void Pause_False()
+    {
+        Pause.Instance.pause_Check = false;
+        Time.timeScale = 1f;
+    }
+    #endregion
 }
+
