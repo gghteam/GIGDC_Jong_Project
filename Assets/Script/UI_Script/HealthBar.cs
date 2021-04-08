16a292be580cf9384ec758c07be46a1a5fc38bcf
Anglik <seonwoo6429@gmail.com>
2021-04-09 00:44:41 +0900

    체력바 구현

---
+++ Assets/Script/UI_Script/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject healthBar;
+    [SerializeField]
+    private Image healthBarImg;
+
+    private void Start()
+    {
+        healthBarImg = GetComponent<Image>();
+    }
+
+    public void UpdateHealthBar(float health, float maxHealth)
+    {
+        healthBarImg.fillAmount = health / maxHealth;
+    }
+}
