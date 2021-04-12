b064e963c42dea7f1ea3420b0902e81bb58fac72
Anglik <seonwoo6429@gmail.com>
2021-04-13 00:30:29 +0900

    TP_UI 구현

---
+++ Assets/Script/UI_Script/TpBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TpBar : MonoBehaviour
+{
+    [SerializeField]
+    private Text tpText = null;
+    [SerializeField]
+    private Image tpBar = null;
+
+    private float curTP;
+    private float maxTP = 100;
+
+    private void Start()
+    {
+        curTP = maxTP;
+    }
+
+    public void UpdateTPBar(float tp)
+    {
+        curTP += tp;
+
+        if (curTP < maxTP)
+        {
+            tpText.text = string.Concat("TP:" + Mathf.Round(curTP));
+            tpBar.fillAmount = curTP / 100;
+        }
+
+    }
+}
