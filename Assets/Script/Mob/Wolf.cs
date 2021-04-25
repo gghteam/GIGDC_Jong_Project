395fe1bc04131a8f0071a9ee00f97d0075ce05c6
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-04-25 23:38:33 +0900

    드디어 늑대 완성

--- Assets/Script/Mob/Wolf.cs
+++ Assets/Script/Mob/Wolf.cs
@@ -10,20 +10,22 @@ public class Wolf : MonoBehaviour
 {
     private Stat stat;
     private AttackRangeCheck check;
-    private BoxCollider2D range;
+
     public float speed = 10f;
     public bool onTarget;
     public bool canMove;
-    private int dir = 1;
+    public bool inWall;
+    public int dir = 1;
+    public Rigidbody2D rigidbady;
 
-    RaycastHit2D hit;
+    //RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbady = GetComponent<Rigidbody2D>();
         check = GetComponentInChildren<AttackRangeCheck>();
         stat = GetComponent<Stat>();
-        range = GetComponent<BoxCollider2D>();
     }
     private void Update()
     {
@@ -32,46 +34,13 @@ public class Wolf : MonoBehaviour
         {
             StartCoroutine(WolfAttack());
         }
+        
+
+        Debug.DrawRay(transform.position, Vector3.right*dir*2.5f, Color.blue, 0.001f);
     }
 
     // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //플레이어가 어그로 범위 안으로 들어오면 인식 범위 늘어남
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            range.size = new Vector2(range.size.x * 2, range.size.y * 2);
-            onTarget = true;
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && !check.canAttack&&canMove)
-        {
-             transform.Translate(Vector2.left * speed * Time.deltaTime);
-            //플레이어가 몹의 위치보다 오른쪽에 있다면 오른쪽 바라보게
-            if (collision.gameObject.transform.position.x > transform.position.x)
-            {
-                transform.rotation = new Quaternion(0, 180, 0, 0);
-                dir = 1;
-            }
-            //플레이어가 몹의 위치보다 왼쪽에 있다면 왼쪽 바라보게
-            else
-            {
-                transform.rotation = new Quaternion(0, 0, 0, 0);
-                dir = -1;
-            }
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        //플레이어가 어그로 범위 밖으로 나가면 인식 범위 줄어듬
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            range.size = new Vector2(3f, 1.5f);
-            onTarget = false;
-        }
-    }
+  
     IEnumerator WolfAttack()
     {
         check.canAttack = false;
@@ -82,25 +51,11 @@ public class Wolf : MonoBehaviour
         //점프 공격들
 
         //transform.DOJump(new Vector3(transform.position.x + 20 * dir, 2, 0), 2, 1, 1f); // 이건 점프하는거 
-        for (int i = 0; i < 10; i++)
-        {
-            Debug.DrawRay(transform.position, Vector3.forward, Color.blue, 0.3f);
-            if (Physics2D.Raycast(transform.position, Vector3.right,10, LayerMask.GetMask("Platform")))
-            {
-                Debug.Log("AS");
-                if (hit.transform.gameObject.CompareTag("Ground"))
-                {
-                    Debug.Log("ASD");
-                   // canMove = false;
-                    break;
-                }
-            }
-           transform.DOBlendableLocalMoveBy(new Vector3(1 * dir, 0, 0), 1);               // 이건 돌진하는거 - 원하는대로 바꿔쓰세연  
-        }
-        
-
-
+        //transform.DOBlendableLocalMoveBy(new Vector3(3 * dir, 0, 0), 1f);
+        rigidbady.AddForce(Vector2.right*dir*15,ForceMode2D.Impulse);
+        rigidbady.AddForce(Vector2.up * 15, ForceMode2D.Impulse);
         Debug.Log("공격");
+        yield return new WaitForSeconds(1f);
         canMove = true;
 
     }
---
+++ Assets/Script/Mob/WolfRange.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfRange : MonoBehaviour
+{
+    public Wolf wolf;
+    public AttackRangeCheck check;
+    public GameObject wolfObj;
+    public BoxCollider2D range;
+    private void Start()
+    {
+        wolf = GetComponentInParent<Wolf>();
+        check = GetComponentInChildren<AttackRangeCheck>();
+        range = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //플레이어가 어그로 범위 안으로 들어오면 인식 범위 늘어남
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            range.size = new Vector2(range.size.x * 2,range.size.y * 2);
+            wolf.onTarget = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !check.canAttack && wolf.canMove)
+        {
+
+            if (!wolf.inWall)
+                wolfObj.transform.Translate(Vector2.left * wolf.speed * Time.deltaTime);
+            //플레이어가 몹의 위치보다 오른쪽에 있다면 오른쪽 바라보게
+            if (collision.gameObject.transform.position.x > transform.position.x)
+            {
+                wolfObj.transform.rotation = new Quaternion(0, 180, 0, 0);
+                wolf.dir = 1;
+            }
+            //플레이어가 몹의 위치보다 왼쪽에 있다면 왼쪽 바라보게
+            else
+            {
+                wolfObj.transform.rotation = new Quaternion(0, 0, 0, 0);
+                wolf.dir = -1;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //플레이어가 어그로 범위 밖으로 나가면 인식 범위 줄어듬
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            range.size = new Vector2(3f, 1.5f);
+            wolf.onTarget = false;
+        }
+    }
+}
