0f1aea8d5b987679e0de68c8f4ea06b0f851a900
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-05-02 01:48:35 +0900

    늑대 시야각

---
+++ Assets/Editor/FOVEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Wolf))]
+
+public class FOVEditor : Editor
+{
+	private void OnSceneGUI()
+	{
+		Wolf fov = (Wolf)target;
+		Vector2 fromAngle = fov.CirclePoint(-fov.viewAngle * 0.5f);
+		Handles.color = Color.white;
+		Handles.DrawWireDisc(fov.transform.position, Vector3.forward, fov.viewRange);
+		Handles.color = new Color(1, 1, 1, 0.2f);
+		Handles.DrawSolidArc(fov.transform.position, Vector3.forward, fromAngle, fov.viewAngle, fov.viewRange);
+		//GUIStyle style = new GUIStyle();
+		//style.fontSize = 35;
+		//Handles.Label(fov.transform.position + new Vector3(0, 0.5f, 0), fov.viewAngle.ToString(), style);
+	}
+}
--- Assets/Script/Mob/Wolf/Wolf.cs
+++ Assets/Script/Mob/Wolf/Wolf.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Wolf : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float viewRange = 10f;
+    [Range(0, 360)]
+    public float viewAngle = 30f;
+    public LayerMask layerMask;
+    private int playerLayer;
+    private void Awake()
+	{
+        playerLayer = LayerMask.NameToLayer("Player");
+	}
+	void Start()
     {
         
     }
@@ -15,4 +23,37 @@ public class Wolf : MonoBehaviour
     {
         
     }
+    public Vector2 CirclePoint(float angle)
+    {
+        return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+    }
+    public bool IsViewPlayer()
+    {
+        bool isView = false;
+        Vector2 dir = GameManager.Player.position - transform.position;
+
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, dir.normalized, viewRange, layerMask);
+        if (hit2D.collider != null)
+        {
+            isView = (hit2D.collider.gameObject.CompareTag("Player"));
+        }
+
+        return isView;
+    }
+    //public bool IsTracePlayer()
+    //{
+    //    bool isTrace = false;
+    //    Collider2D col = Physics2D.OverlapCircle(transform.position, viewRange, 1 << playerLayer);
+    //    if (col != null)
+    //    { //서클안에 충돌체가 있다면 거리 측정
+    //        Vector2 dir = GameManager.Player.position - transform.position; //z를 무시하고 받기 위해 Vector2로 형변환 한다.
+    //        //Vector3 right = enemyMove.facingRight ? transform.right : transform.right * -1;
+
+    //        if (Vector2.Angle(right, dir) < viewAngle * 0.5f)
+    //        {
+    //            isTrace = true;
+    //        }
+    //    }
+    //    return isTrace;
+    //}
 }
