6bb46f923083f9a71190d31e351e76b2a2364b6f
DESKTOP-77DPHAE\ROG <3soojung@gmail.com>
2021-05-05 12:12:51 +0900

    늑대 인식 수정

--- Assets/Script/Mob/Wolf/EnemyFOV.cs
+++ Assets/Script/Mob/Wolf/EnemyFOV.cs
@@ -23,28 +23,32 @@ public class EnemyFOV : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        IsViewPlayer();
-        IsTracePlayer();
+        if (IsTracePlayer())
+        {
+            IsViewPlayer();
+        }
     }
     public Vector2 CirclePoint(float angle)
     {
         return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
     }
+    //플레이어와 나 사이에 다른 장애물이 없는가?
     public bool IsViewPlayer()
     {
         bool isView = false;
         Vector2 dir = GameManager.Player.position - transform.position;
 
-        RaycastHit2D hit2D = Physics2D.Raycast(transform.position,new Vector2(-1*transform.localScale.x,0), viewRange,layerMask);
-        if (hit2D.collider != null&&GameManager.GetScale() != 0)
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, dir.normalized, viewRange, layerMask);
+        Debug.DrawRay(transform.position, dir.normalized * viewRange, Color.red, 0.1f);
+        if (hit2D.collider != null)
         {
             isView = (hit2D.collider.gameObject.CompareTag("Player"));
-            Debug.Log(hit2D.collider.gameObject.name);
+            Debug.Log(isView);
         }
 
         return isView;
     }
-	public bool IsTracePlayer()
+    public bool IsTracePlayer()
 	{
 		bool isTrace = false;
 		Collider2D col = Physics2D.OverlapCircle(transform.position, viewRange, 1 << playerLayer);
